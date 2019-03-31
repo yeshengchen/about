7bac90afa1f86d8f81015b54c8b511f12f96158b
Chauncy Chen <yesheng.me@gmail.com>
2019-03-31 13:16:19 +0800

    增加编辑功能

--- about.py
+++ about.py
@@ -10,22 +10,33 @@ from AppKit import NSWorkspace
 ROOT_PATH = sys.path[0]
 DATA_PATH = sys.path[0] + '/data/'
 
+def opApp(filePath, op):
+  if op == '':
+    print os.system("clear")
+    print os.system("less "+filePath)
+  elif op == '-edit':
+    os.system("vim "+filePath)
+  else:
+    pass
+
 def main():
-  op = ''
+  app = ''
   if len(sys.argv) > 1:
-    op = sys.argv[1]
+    app = sys.argv[1]
   else:
     # 没有输入,获取当前程序
-    op = NSWorkspace.sharedWorkspace().frontmostApplication().localizedName() 
+    app = NSWorkspace.sharedWorkspace().frontmostApplication().localizedName() 
     pass
-  
-  filePath = DATA_PATH+op
+
+  op = '' 
+  if len(sys.argv) > 2:
+    op = sys.argv[2]
+
+  filePath = DATA_PATH + app
   if os.path.exists(filePath):
-    print os.system("clear")
-    print os.system("less "+DATA_PATH+op)
+    opApp(filePath, op)
   else:
-    print '当前程序:'+op+',没有找到配置' 
+    print '当前程序:'+app+',没有找到配置' 
+
     
 main()
-
-
